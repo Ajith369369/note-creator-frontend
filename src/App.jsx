d9132afaea4760a2a07b29386de625f28bf43a55
Ajith K. S. <ajithks98086@gmail.com>
2024-08-17 04:12:59 +0530

    Add import statements.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-//import {HomePage, AddNote, EditNote} from "./pages";
-//import { BrowserRouter, Route, Routes, Link} from "react-router-dom";
-import './App.css'
+import HomePage from "./pages/HomePage";
+import Notes from "./pages/Notes";
+import AddNote from "./pages/AddNote";
+import EditNote from "./pages/EditNote";
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
 
 function App() {
-
   return (
     <>
-     
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<Notes />} />
+        <Route path="/add" element={<AddNote />} />
+        <Route path="/edit" element={<EditNote />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
