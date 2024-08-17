c7b8175a2e37cca3d8162f824a59c30646610952
Ajith K. S. <ajithks98086@gmail.com>
2024-08-18 04:30:10 +0530

    Create API functions.

--- src/services/nc_allApi.js
+++ src/services/nc_allApi.js
@@ -1,24 +1,57 @@
-import { serverUrl } from './nc_serverUrl';
-import { commonApi } from './nc_commonApi';
-
+import { commonApi } from "./nc_commonApi";
+import { serverUrl } from "./nc_serverUrl";
 
 // register
-export const registerApi = async(reqBody) => {
-  return await commonApi('POST', `${serverUrl}/register`, reqBody, "")
+export const registerApi = async (reqBody) => {
+  return await commonApi("POST", `${serverUrl}/register`, reqBody, "");
 };
 
 // login
-export const loginApi = async(reqBody) => {
-  return await commonApi('POST', `${serverUrl}/login`, reqBody, "")
+export const loginApi = async (reqBody) => {
+  return await commonApi("POST", `${serverUrl}/login`, reqBody, "");
 };
 
-// add project
+// add note of a user
 // uploaded content requests need header.
-export const addProjectApi = async(reqBody, reqHeader) => {
-  return await commonApi('POST', `${serverUrl}/add-project`, reqBody, reqHeader)
+export const addNoteApi = async (reqBody, reqHeader) => {
+  return await commonApi(
+    "POST",
+    `${serverUrl}/notes/user/add`,
+    reqBody,
+    reqHeader
+  );
 };
 
-// all project
-export const allProjectApi = async() => {
-  return await commonApi('GET', `${serverUrl}/all-project`, "", "")
+// get all notes from all users
+export const getAllNotesOfAllUsersApi = async () => {
+  return await commonApi("GET", `${serverUrl}/notes/all`, "", "");
+};
+
+// get all notes of a user
+export const getAllNotesOfAUserApi = async (reqHeader) => {
+  return await commonApi("GET", `${serverUrl}/notes/user/all`, "", reqHeader);
+};
+
+// edit note of a user
+export const editNoteApi = async (noteId, reqBody, reqHeader) => {
+  //id is passed as path parameter
+  return await commonApi(
+    "PUT",
+    `${serverUrl}/notes/user/edit/${noteId}`,
+    reqBody,
+    reqHeader
+  );
+};
+
+// delete note of a user
+// {} (Empty Object) for reqBody: In most cases, for DELETE requests, it's common to send an empty object as the body if the library or framework expects a body parameter, even if it's not typically used in DELETE requests. Some libraries or implementations might require an object, even if it's empty.
+// "" (Empty String) for reqBody: Using an empty string as the body for a DELETE request is not standard practice and may not be handled correctly by the API client or server. Most HTTP libraries expect either null or an empty object ({}) when there is no body content.
+// Standard Practice: It is more conventional to use {} for DELETE requests when the body is expected to be an object. This is especially true if you are using a library or API client that expects an object for consistency.
+export const deleteNoteApi = async (noteId, reqHeader) => {
+  return await commonApi(
+    "DELETE",
+    `${serverUrl}/notes/user/delete/${noteId}`,
+    {},
+    reqHeader
+  );
 };
