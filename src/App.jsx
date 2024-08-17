e97220da23abb5e4b6fbaf7901374b01b01c48ba
Ajith K. S. <ajithks98086@gmail.com>
2024-08-17 16:51:27 +0530

    Add Auth.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import AddNote from "./pages/AddNote";
 import EditNote from "./pages/EditNote";
 import HomePage from "./pages/HomePage";
 import Notes from "./pages/Notes";
+import Auth from "./pages/Auth";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Auth register/>} />
+        <Route path="/login" element={<Auth />} />
         <Route path="/profile-home" element={<HomePage />} />
         <Route path="/home" element={<Notes />} />
         <Route path="/add" element={<AddNote />} />
