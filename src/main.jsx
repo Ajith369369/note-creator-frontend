ac86b64f33dee84ab233a77e2292ad51a647f165
Ajith K. S. <ajithks98086@gmail.com>
2024-08-17 17:52:57 +0530

    Fix padding.

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./bootstrap.min.css";
 import "./index.css";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}><App /></Provider>
     </BrowserRouter>
   </StrictMode>
 );
