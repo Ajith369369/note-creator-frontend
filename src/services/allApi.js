96faef5843b5bf9781316810f70c69d2e0ac36ce
Ajith K. S. <ajithks98086@gmail.com>
2024-08-17 16:56:13 +0530

    Add services.

---
+++ src/services/allApi.js
@@ -0,0 +1,29 @@
+import { serverUrl } from './serverUrl';
+import { commonApi } from './commonApi';
+
+
+// register
+export const registerApi = async(reqBody) => {
+  return await commonApi('POST', `${serverUrl}/register`, reqBody, "")
+};
+
+// login
+export const loginApi = async(reqBody) => {
+  return await commonApi('POST', `${serverUrl}/login`, reqBody, "")
+};
+
+// add project
+// uploaded content requests need header.
+export const addProjectApi = async(reqBody, reqHeader) => {
+  return await commonApi('POST', `${serverUrl}/add-project`, reqBody, reqHeader)
+};
+
+// home project
+export const homeProjectApi = async() => {
+  return await commonApi('GET', `${serverUrl}/home-project`, "", "")
+};
+
+// all project
+export const allProjectApi = async() => {
+  return await commonApi('GET', `${serverUrl}/all-project`, "", "")
+};
