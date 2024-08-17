103ef66b8bf3d102fd84c8bd3cfade82a50b25da
Ajith K. S. <ajithks98086@gmail.com>
2024-08-17 15:33:21 +0530

    Create Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import HomePage from "./pages/HomePage";
-import Notes from "./pages/Notes";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import "./App.scss";
 import AddNote from "./pages/AddNote";
 import EditNote from "./pages/EditNote";
-import { Route, Routes } from "react-router-dom";
-import "./App.scss";
+import HomePage from "./pages/HomePage";
+import Notes from "./pages/Notes";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/profile-home" element={<HomePage />} />
         <Route path="/home" element={<Notes />} />
         <Route path="/add" element={<AddNote />} />
         <Route path="/edit" element={<EditNote />} />
