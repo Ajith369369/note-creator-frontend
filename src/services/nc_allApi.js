ef0dace223a4e55b43d5dace64e444fac09a42eb
Ajith K. S. <ajithks98086@gmail.com>
2024-08-18 06:25:43 +0530

    Change name.

--- src/services/nc_allApi.js
+++ src/services/nc_allApi.js
@@ -33,7 +33,7 @@ export const getAllNotesOfAUserApi = async (reqHeader) => {
 };
 
 // edit note of a user
-export const editNoteApi = async (noteId, reqBody, reqHeader) => {
+export const editNoteOfAUserApi = async (noteId, reqBody, reqHeader) => {
   //id is passed as path parameter
   return await commonApi(
     "PUT",
@@ -47,7 +47,7 @@ export const editNoteApi = async (noteId, reqBody, reqHeader) => {
 // {} (Empty Object) for reqBody: In most cases, for DELETE requests, it's common to send an empty object as the body if the library or framework expects a body parameter, even if it's not typically used in DELETE requests. Some libraries or implementations might require an object, even if it's empty.
 // "" (Empty String) for reqBody: Using an empty string as the body for a DELETE request is not standard practice and may not be handled correctly by the API client or server. Most HTTP libraries expect either null or an empty object ({}) when there is no body content.
 // Standard Practice: It is more conventional to use {} for DELETE requests when the body is expected to be an object. This is especially true if you are using a library or API client that expects an object for consistency.
-export const deleteNoteApi = async (noteId, reqHeader) => {
+export const deleteNoteOfAUserApi = async (noteId, reqHeader) => {
   return await commonApi(
     "DELETE",
     `${serverUrl}/notes/user/delete/${noteId}`,
