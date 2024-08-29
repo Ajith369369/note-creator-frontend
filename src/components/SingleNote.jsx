b2de100ec9241f3e2f66b535890f784572b4e267
Ajith K. S. <ajithks98086@gmail.com>
2024-08-30 05:06:55 +0530

    Get state in SingleNote.jsx

--- src/components/SingleNote.jsx
+++ src/components/SingleNote.jsx
@@ -2,20 +2,25 @@ import React from "react";
 import { useSelector } from "react-redux";
 // import { getAllNotes } from "../../features/notes/noteSlice";
 import "./SingleNote.scss";
+import { useLocation } from "react-router-dom";
 // import { useParams } from 'react-router';
 
 const SingleNotePage = () => {
+
+  const location= useLocation()
+  const selectedNote = location.state?.selectedNote;
+  console.log("selectedNote: ", selectedNote);
   // const { id } = useParams();
   // const notes = useSelector(getAllNotes);
   // let tempNote = notes.filter((note) => note.noteId === id);
   return (
     <section className="note-single-section px-4">
       <div className="note-single-title">
-        {/* <h2 className="my-2 fs-20">{tempNote[0].noteTitle}</h2> */}
+        <h2 className="my-2 fs-20">{selectedNote?.noteTitle}</h2>
       </div>
 
       <div className="py-4">
-        {/* <p>{tempNote[0].noteContent}</p> */}
+        <p>{selectedNote?.noteContent}</p>
       </div>
     </section>
   );
