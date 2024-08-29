dfd9e67cef08a477ccf94cfb632ef098a8c15221
Ajith K. S. <ajithks98086@gmail.com>
2024-08-30 04:48:49 +0530

    Add SingleNote.jsx to Routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import SingleNote from "../components/SingleNote";
 import "./App.scss";
 import AddNote from "./pages/AddNote";
+import Auth from "./pages/Auth";
 import EditNote from "./pages/EditNote";
+import Home from "./pages/Home";
 import HomePage from "./pages/HomePage";
 import Notes from "./pages/Notes";
-import Auth from "./pages/Auth";
 import NotesPage from "./pages/NotesPage";
-
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Auth register/>} />
+        <Route path="/register" element={<Auth register />} />
         <Route path="/login" element={<Auth />} />
         <Route path="/home" element={<Notes />} />
-        <Route path="/profile-home" element={<HomePage />} >
+        <Route path="/profile-home" element={<HomePage />}>
           <Route path="/profile-home/notes" element={<NotesPage />} />
           <Route path="/profile-home/add" element={<AddNote />} />
           <Route path="/profile-home/edit" element={<EditNote />} />
-          <Route path = "/profile-home/note" element = {<SingleNote />} />
-          </Route>
+          <Route path="/profile-home/note" element={<SingleNote />} />
+        </Route>
       </Routes>
     </>
   );
