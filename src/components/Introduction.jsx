0d8a6cfb874a1a03bb8c03ac8261196f952928eb
Ajith K. S. <ajithks98086@gmail.com>
2024-09-12 13:57:52 +0530

    Fix text decoration.

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Introduction.scss";
 
 function Introduction() {
   return (
-    <>Introduction</>
-  )
+    <>
+      <h1 className="text-center">The simplest way to keep notes</h1>
+      <h4 className="text-center">All your notes in one place</h4>
+
+      <div className="row w-100 m-0 my-4">
+        <Link to={"/profile-home/add"} className="flex justify-center align-center">
+          <button type="button" className="btn btn-default" style={{textDecoration:'none'}}>
+            Create A Note
+          </button>
+        </Link>
+      </div>
+    </>
+  );
 }
 
-export default Introduction
+export default Introduction;
