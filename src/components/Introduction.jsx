a56237271d9196cf23d8241b89bc744878664d9e
Ajith K. S. <ajithks98086@gmail.com>
2024-09-12 14:02:37 +0530

    Create button.

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -5,15 +5,19 @@ import "./Introduction.scss";
 function Introduction() {
   return (
     <>
-      <h1 className="text-center">The simplest way to keep notes</h1>
+      <h1 className="text-center mt-3">The simplest way to keep notes</h1>
       <h4 className="text-center">All your notes in one place</h4>
 
-      <div className="row w-100 m-0 my-4">
+      <div className="row w-100 m-0 my-5">
         <Link to={"/profile-home/add"} className="flex justify-center align-center">
-          <button type="button" className="btn btn-default" style={{textDecoration:'none'}}>
-            Create A Note
+          <button type="button" className="btn btn-default p-4 rounded-pill">
+            <h3>Create A Note</h3>
           </button>
         </Link>
+      </div>
+
+      <div className="row w-100 m-0 my-5">
+        
       </div>
     </>
   );
