9b904923699b6e08420b398c621d83f5cc1e6d16
Ajith K. S. <ajithks98086@gmail.com>
2024-09-15 04:51:02 +0530

    Scroll to top.

--- src/pages/AddNote.jsx
+++ src/pages/AddNote.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import AddNoteForm from "../components/AddNoteForm";
 import "./AddNote.scss";
 
 function AddNote() {
+
+   // Scroll to the top on component mount
+   useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <AddNoteForm />
