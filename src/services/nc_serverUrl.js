aca945c09363c037bb7dd82681bae05c83c4602b
Ajith K. S. <ajithks98086@gmail.com>
2024-09-26 17:02:21 +0530

    Change url.

--- src/services/nc_serverUrl.js
+++ src/services/nc_serverUrl.js
@@ -1,2 +1,4 @@
 // Base URL of the backend server
-export const serverUrl = 'http://localhost:3500'
+// export const serverUrl = 'http://localhost:3500'
+
+export const serverUrl = 'https://note-creator-backend.onrender.com'
