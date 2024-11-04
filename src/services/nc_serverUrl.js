df02bd141d51c7c3e99a62b009d05cf3342be19b
Ajith K. S. <ajithks98086@gmail.com>
2024-11-04 11:42:32 +0530

    Add comment.

--- src/services/nc_serverUrl.js
+++ src/services/nc_serverUrl.js
@@ -2,3 +2,14 @@
 // export const serverUrl = 'http://localhost:3500'
 
 export const serverUrl = 'https://note-creator-backend.onrender.com'
+
+// #region Multi-line Comment
+/**
+ * <img src={`${serverUrl}/uploads/${selectedNote?.noteImage}`} alt="" />
+ * 
+ * http://localhost:3500/uploads/image-1727776798480-login-form-image.jpg
+ * 
+ * https://note-creator-backend.onrender.com/uploads/image-1727776798480-login-form-image.jpg
+ * 
+ */
+// #endregion
