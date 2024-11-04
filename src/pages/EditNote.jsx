7bc6064144daee31f1df69e128541e978612beab
Ajith K. S. <ajithks98086@gmail.com>
2024-11-04 11:29:13 +0530

    Add scroll.

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,10 +1,15 @@
+import { useEffect } from "react";
 import EditNoteForm from "../components/EditNoteForm";
 import useAuthGuard from "./useAuthGuard";
 
 function EditNote() {
-
   // Check if user is authenticated
   useAuthGuard();
+
+  // Scroll to the top on component mount
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>
