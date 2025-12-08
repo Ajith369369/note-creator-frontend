f9136cce29664d0e7f961d34bb55f9af0ab86c95
Ajith K. S. <ajithks98086@gmail.com>
2025-12-08 16:17:39 +0530

    Remove bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./bootstrap.min.css";
 import "./index.css";
 import store from "./redux/store";
-import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Provider store={store}><App /></Provider>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
